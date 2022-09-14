4db32cad93fc0cc92457a7acb8c6c3e49fea7c8a
ana luz martinez <almadigital.mx@gmail.com>
2022-09-13 23:40:34 -0700

    journal table done, need to add create, edit, and delete

--- src/components/JournalCard/JournalCard.jsx
+++ src/components/JournalCard/JournalCard.jsx
@@ -1,16 +1,32 @@
-import {
-  Avatar,
-  Box,
-  Card,
-  CardContent,
-  CardHeader,
-  CardMedia,
-  IconButton,
-  Typography,
-} from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { styled } from '@mui/material/styles';
+import { TableCell, tableCellClasses, TableRow,  } 
+from "@mui/material";
+import StarIcon from '@mui/icons-material/Star';
 
-const JournalCard = ({journalEntry}) => {
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+
+
+const JournalCard = ({journalEntry, title, description, coverUrl, isFeatured, date}) => {
     const navigate = useNavigate();
 
   const handleEdit = () => {
@@ -21,8 +37,18 @@ const JournalCard = ({journalEntry}) => {
 
   };
 
-  return (
-    <div>JournalCard</div>
+
+  return (<>
+            <StyledTableRow key={date}>
+              <StyledTableCell component="th" scope="row">
+                {date}
+              </StyledTableCell>
+              <StyledTableCell align="left">{description}</StyledTableCell>
+              <StyledTableCell align="right">{isFeatured? <StarIcon/>:null}</StyledTableCell>
+            </StyledTableRow>
+          
+</>
+
   )
 }
 
--- src/components/JournalEntries/JournalEntries.jsx
+++ src/components/JournalEntries/JournalEntries.jsx
@@ -1,7 +1,23 @@
 import { useState, useEffect } from "react";
+import { styled } from '@mui/material/styles';
+import { CircularProgress, TableCell, tableCellClasses, TableRow, TableContainer, Paper, Table, TableHead, Container, TableBody } from "@mui/material";
 import { getJournalEntriesByUserWs } from "../../services/journal-ws"
 import { useNavigate } from "react-router-dom";
+import {} from "@mui/material/";
+import JournalCard from "../JournalCard/JournalCard";
 
+//table looks
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+//
 const JournalEntries = (props) => {
     const navigate = useNavigate();
     const [isLoading, setIsLoading] = useState(true);
@@ -22,24 +38,47 @@ const JournalEntries = (props) => {
     findJournalEntry();
   }, []);
 
-
+//journal card map
   function userJournalsList() {
-   return journalEntries.map((journal, i) => {
+   return [...journalEntries].reverse().map((journal, i) => {
      return (
-       <li key={i}>
-         {journal.title} a
-       </li>
+          <JournalCard key={i} title={journal.title} description={journal.description} coverUrl={journal.coverUrl} isFeatured={journal.isFeatured} date={journal.date}></JournalCard>
      );
    });
  }
 
   return (
     <>
-      <p>JournalEntries</p>
-      <ul>      {userJournalsList()}</ul>
-  
+<p>JournalEntries</p>
+<Container maxWidth="fixed" >
+<Paper elevation={6} sx={{  display:"flex", 
+alignItems:"center", justifyContent:"center", flexDirection:"column", p:"30px", width:"80%"}} > 
+        {!isLoading && journalEntries.length > 0 ? 
+        (
+          <>
+      <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>DATE</StyledTableCell>
+            <StyledTableCell align="left">TITLE</StyledTableCell>
+            <StyledTableCell align="left"></StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+              {userJournalsList()}
+            </TableBody>
+          </Table>
+        </TableContainer>
           </>
- 
+        ) : (
+          
+          <p><CircularProgress/></p>
+        )}
+
+        </Paper>
+        </Container>
+    </>
   )
 }
 
